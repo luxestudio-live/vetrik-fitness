d13409c9a40970af7fbebd6f8175d3ca03263dad
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2025-12-29 00:39:41 +0530

    Update assetPrefix and basePath for Vetrik Fitness deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  assetPrefix: process.env.GITHUB_PAGES === 'true' ? '/power-gold-gym/' : '',
-  basePath: process.env.GITHUB_PAGES === 'true' ? '/power-gold-gym' : '',
+  assetPrefix: process.env.GITHUB_PAGES === 'true' ? '/vetrik-fitness/' : '',
+  basePath: process.env.GITHUB_PAGES === 'true' ? '/vetrik-fitness' : '',
 }
 
 export default nextConfig
